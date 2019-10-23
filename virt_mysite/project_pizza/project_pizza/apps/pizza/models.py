cfd7308c9e546bce27dab53214fd9599a71c3cb9
YraDokienko <uradokienko@gmail.com>
2019-10-23 18:00:13 +0300

    add class Pizza

--- virt_mysite/project_pizza/project_pizza/apps/pizza/models.py
+++ virt_mysite/project_pizza/project_pizza/apps/pizza/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
