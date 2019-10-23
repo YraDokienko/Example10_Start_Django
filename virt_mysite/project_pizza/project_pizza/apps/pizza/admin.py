475b55a31b78d4eee8305d05fc6c2b9933e3e06d
YraDokienko <uradokienko@gmail.com>
2019-10-24 01:38:39 +0300

    configured admin panel

--- virt_mysite/project_pizza/project_pizza/apps/pizza/admin.py
+++ virt_mysite/project_pizza/project_pizza/apps/pizza/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Pizza, Size
+from .models import Pizza, Size, Ingredient
 
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'size', 'price', 'status']
+    list_display_links = ['name']
+    list_editable = ['price']
+    list_filter = ['size', 'price']
+    search_fields = ['^name']
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price']
+    list_display_links = ['name']
+    list_editable = ['price']
+    list_filter = ['price']
+
+
+admin.site.register(Pizza, PizzaAdmin)
 admin.site.register(Size)
-
+admin.site.register(Ingredient, IngredientAdmin)
--- virt_mysite/project_pizza/project_pizza/apps/pizza/models.py
+++ virt_mysite/project_pizza/project_pizza/apps/pizza/models.py
@@ -8,7 +8,20 @@ class Size(models.Model):
         return self.name
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.name
+
+
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    ingredient = models.ManyToManyField(Ingredient)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
